d32fea9ef7819d172b59e034d407af97eedd62ee
joycehwang <wangj9@email.chop.edu>
2016-07-26 10:11:54 -0400

    Update NBDB_CDK.R

--- NBDB_CDK.R
+++ NBDB_CDK.R
@@ -2,7 +2,7 @@ library(RNeo4j)
 
 graph = startGraph("http://localhost:7474/db/data/", username = "neo4j", password = "NEO$J")
 
-#number of CDK sensitive cell lines (sentivity defined as IC50 < 1000 nanomoles)
+#number of CDK sensitive cell lines (sentivity defined as IC50 < 1000 nM)
 query = "
 MATCH (c:CellLine)<-[r:IC50]-(a:Agent)-[:TARGETS]->(:Gene {name:'CDK4'})
 WHERE r.score < 1000
@@ -17,6 +17,8 @@ WHERE r.score > 1000
 RETURN COUNT(c) AS count
 "
 RES_CL = as.numeric(cypher(graph,query))
+
+#################################################################################
 
 #cell lines sensitive to CDK inhibitors 
 query = "
@@ -52,6 +54,47 @@ ggplot(data = CDK_RES, aes(Count)) +
   geom_freqpoly(binwidth = 0.2) +
   labs(title = "Gene Count in CDK Resistent Cell Lines", x = "Gene Count", y = "Frequency")
 
+#################################################################################
+
+#scatterplot of resistent counts vs sensitive counts for genes
+intSets <- intersect(CDK_RES[,1], CDK_SENS[,1])
+rownames(CDK_RES) <- CDK_RES[,1]
+rownames(CDK_SENS) <- CDK_SENS[,1]
+DF_RES_SENS_GENE <- cbind(CDK_RES[intSets,], CDK_SENS[intSets,2]);
+colnames(DF_RES_SENS_GENE) <- c("Gene", "Resistant_Count", "Sensitive_Count");
+grep(DF_RES_SENS_GENE[,1], "CDK");
+DF_RES_SENS_GENE[,"CDK_SET"] <- grepl("CDK", DF_RES_SENS_GENE[,1]);
+ggplot(DF_RES_SENS_GENE, aes(Resistant_Count, Sensitive_Count, color=CDK_SET)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  theme_bw();
+ggplot(DF_RES_SENS_GENE, aes(Resistant_Count, Sensitive_Count, color=CDK_SET, size=CDK_SET)) +
+  geom_point() +
+  theme_bw();
+
+#sensitive:resistent counts ratio
+DF_RES_SENS_GENE[,"ResSensRatio"] <- DF_RES_SENS_GENE["Resistant_Count"]/DF_RES_SENS_GENE[,"Sensitive_Count"];
+DF_RES_SENS_GENE <- DF_RES_SENS_GENE[order(DF_RES_SENS_GENE[,"ResSensRatio"]),]
+
+hist(DF_RES_SENS_GENE[,"ResSensRatio"], breaks = 20, main = "CDK Resistant:Sensitive Gene Ratios", xlab = "Resistant:Sensitive Ratio")
+DF_RES_SENS_GENE[DF_RES_SENS_GENE[,"CDK_SET"] == T,]
+
+#log transform ratios to normalize data
+LOG_TRANS_RATIO <- log(DF_RES_SENS_GENE$ResSensRatio)
+CDK_LOG_TRANS <- cbind(DF_RES_SENS_GENE$Gene, LOG_TRANS_RATIO)
+hist(LOG_TRANS_RATIO, breaks = 20, main = "CDK Resistant:Sensitive Gene Ratios", xlab = "ln(Resistant:Sensitive Ratio)")
+
+#################################################################################
+
+#look at MYCN
+query = "
+MATCH (g:Gene {name:'MYCN'})-[r]-(x)
+RETURN x.name AS Name, labels(x) AS Type, type(r) AS Relationship
+"
+MYCN = cypher(graph,query)
+
+#################################################################################
+
 #pathways affected in sensitive cell lines
 query = "
 MATCH (c:CellLine)<-[r:IC50]-(a:Agent)-[:TARGETS]->(:Gene {name:'CDK4'}),
@@ -77,12 +120,14 @@ RETURN p.name AS Pathway, count(p) AS Count
 ORDER BY Count DESC
 "
 CDK_RES_PATH = cypher(graph,query)
-CDK_RENS_PATH$Count = CDK_RES_PATH$Count/RES_CL
+CDK_RES_PATH$Count = CDK_RES_PATH$Count/RES_CL
 
 ggplot(data = CDK_RES_PATH, aes(Count)) +
   geom_histogram(binwidth = 1) +
   geom_freqpoly(binwidth = 1) +
   labs(title = "Pathway Count in CDK Resistent Cell Lines", x = "Pathway Count", y = "Frequency")
+
+#################################################################################
 
 #scatterplot of resistent counts vs sensitive counts
 intSets <- intersect(CDK_RES_PATH[,1], CDK_SENS_PATH[,1])
@@ -95,14 +140,34 @@ DF_RES_SENS_PATH[,"CDK_SET"] <- grepl("CDK", DF_RES_SENS_PATH[,1]);
 ggplot(DF_RES_SENS_PATH, aes(Resistant_Count, Sensitive_Count, color=CDK_SET)) +
     geom_point() +
     geom_smooth(method="lm") +
-    theme_bw();
+    theme_bw() +
+    labs(title = "CDK Sensitive and Resistant Pathway Counts", x = "Resistant Count", y = "Sensitive Count") +
+    guides(color = guide_legend(title = "CDK"));
 ggplot(DF_RES_SENS_PATH, aes(Resistant_Count, Sensitive_Count, color=CDK_SET, size=CDK_SET)) +
     geom_point() +
-    theme_bw();
+    theme_bw() +
+    labs(title = "CDK Sensitive and Resistant Pathway Counts", x = "Resistant Count", y = "Sensitive Count") +
+    guides(color = guide_legend(title = "CDK"), size = guide_legend(title = "CDK"));
 
 #sensitive:resistent counts ratio
-DF_RES_SENS_PATH[,"SensResRatio"] <- DF_RES_SENS_PATH["Resistant_Count"]/DF_RES_SENS_PATH[,"Sensitive_Count"];
-DF_RES_SENS_PATH <- DF_RES_SENS_PATH[order(DF_RES_SENS_PATH[,"SensResRatio"]),]
+DF_RES_SENS_PATH[,"ResSensRatio"] <- DF_RES_SENS_PATH["Resistant_Count"]/DF_RES_SENS_PATH[,"Sensitive_Count"];
+DF_RES_SENS_PATH <- DF_RES_SENS_PATH[order(DF_RES_SENS_PATH[,"ResSensRatio"]),]
 
-hist(DF_RES_SENS_PATH[,"SensResRatio"], breaks=100)
-DF_RES_SENS_PATH[DF_RES_SENS_PATH[,"CDK_SET"]==T,]
+hist(DF_RES_SENS_PATH[,"ResSensRatio"], breaks = 100, main = "CDK Resistant:Sensitive Pathway Ratios", xlab = "Resistant:Sensitive Ratio")
+DF_RES_SENS_PATH[DF_RES_SENS_PATH[,"CDK_SET"] == T,]
+
+#################################################################################
+
+#log transform ratios to normalize data
+LOG_TRANS_RATIO <- log(DF_RES_SENS_PATH$ResSensRatio)
+CDK_LOG_TRANS <- cbind(DF_RES_SENS_PATH$Pathway, LOG_TRANS_RATIO)
+hist(LOG_TRANS_RATIO, breaks = 150, main = "CDK Resistant:Sensitive Pathway Ratios", xlab = "ln(Resistant:Sensitive Ratio)")
+
+#################################################################################
+
+#look at MOLENAAR_TARGETS_OF_CCND1_AND_CDK4_UP
+query = "
+MATCH (p:Pathway {name:'MOLENAAR_TARGETS_OF_CCND1_AND_CDK4_UP'})-->(g:Gene)-[r]->(c:CellLine)
+RETURN g.name AS Gene, type(r) AS Relationship, c.name AS CellLine
+"
+cypher(graph,query)
